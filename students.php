b06277df18f4991ee6725ff33477d06e980e2fd3
Yohani Abeykoon <80519190+Yohaniii25@users.noreply.github.com>
2023-02-21 10:48:07 +0530

    Update students.php

--- students.php
+++ students.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Ajax crud 01</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
 
@@ -78,6 +78,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ></script>
 
     <script>
+        //Create a student      
         $(document).on('submit', '#saveStudent', function (e) {
             e.preventDefault();
 
